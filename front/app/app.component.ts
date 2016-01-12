f308eed6924bbec9b84ec01f2aa13bf71d885dc3
Thomas Hull <thull@scottlogic.co.uk>
2016-01-12 11:28:23 +0000

    Purged dead proect ideas

--- front/app/app.component.ts
+++ front/app/app.component.ts
@@ -1,10 +1,6 @@
 import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core'
-import {Todo} from './todos/models/todo'
-import {NewTodoFormComponent} from './todos/form/new-todo-form.component'
-import {WeekPlannerComponent} from './planner/component/week/week-planner.component'
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import {TodoDashboardComponent} from './todos/todo-dashboard.component'
 import {GameComponent} from './numbers/component/game/game.component'
 
 
@@ -12,8 +8,6 @@ import {GameComponent} from './numbers/component/game/game.component'
     selector: 'my-app',
     template: `
     <a [routerLink]="['Dashboard']">Dashboard</a>
-    <a [routerLink]="['Week', {index:0}]">This Week</a>
-    <a [routerLink]="['Week', {index:1}]">This Week</a>
     <router-outlet></router-outlet>
     `,
     styleUrls:['app/styles/style.css'],
@@ -21,11 +15,10 @@ import {GameComponent} from './numbers/component/game/game.component'
 })
 
 @RouteConfig([
-    { path: '/week/:index', name: 'Week', component: WeekPlannerComponent },
     {path:'/',      name: 'Dashboard',   component: GameComponent, useAsDefault: true}
 ])
 export class AppComponent {
-    public title = 'Meal Planner';
+    public title = 'Number Funk';
 
 
 }
--- front/app/boot.ts
+++ front/app/boot.ts
@@ -1,9 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
-import {TodoService} from './todos/service/todo.service'
-import {DayService} from './planner/service/day.service'
 import {NumberService} from './numbers/service/number.service'
 import {ROUTER_PROVIDERS} from 'angular2/router'
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-bootstrap(AppComponent, [TodoService, DayService, NumberService, ROUTER_PROVIDERS, HTTP_PROVIDERS]);
+bootstrap(AppComponent, [NumberService, ROUTER_PROVIDERS, HTTP_PROVIDERS]);
